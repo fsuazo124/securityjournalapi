9b88cfd254fdc5809014b53d4914bbc27ea95fc8
FerSu <fsuazo124@gmail.com>
2024-03-09 18:10:39 -0600

    FS: +Habilitar cors

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  // Opciones de configuración CORS (opcional)
+  const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  // Habilitar CORS con opciones
+  app.enableCors(corsOptions);
+
   //Prefijo de las rutas 
   app.setGlobalPrefix('sj/api')
-  
-
 
   //Validacion de Pipes Global
-  app.useGlobalPipes( 
+  app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
   );
-  
+
   //
   await app.listen(process.env.API_PORT);
 }
