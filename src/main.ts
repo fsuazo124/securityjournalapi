c1831a2a94dc321684a835c971be2c81dac3381c
FerSu <fsuazo124@gmail.com>
2024-03-22 17:02:07 -0600

    FS: +Correcion de error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // Opciones de configuración CORS (opcional)
   const corsOptions = {
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
 
