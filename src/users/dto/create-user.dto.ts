a3c2e3c2be27f47061c2ff86c021f6ad14db19e0
FerSu <fsuazo124@gmail.com>
2024-03-31 20:54:44 -0600

    FS: +Correcciones respuestas de endpoints

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,28 +3,28 @@ import { IsString, IsNotEmpty, IsInt, Matches, Min, Length } from 'class-validat
 export class CreateUserDto {
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({message: "El nombre de usuario no puede estar vacio"})
   @Length(4, 15)
   user_name: string;
 
   @IsString()
-  @IsNotEmpty()
-  @Length(8, 20)
+  @IsNotEmpty({message: "La contraseña no puede estar vacia"})
+  @Length(8, 20,{message: "La contraseña debe contener al menos 8 caracteres"})
   @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/, {
     message:
-      'The password must contain at least one capital letter, one number, and one special character',
+      'La contraseña debe contener al menos una letra mayúscula, un número y un carácter especial',
   })
   password: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({message: "El campo primer nombre no puede estar vacio"})
   first_name: string;
 
   @IsString()
   last_name?: string;
 
   @IsInt()
-  @IsNotEmpty()
+  @IsNotEmpty({message: "El campo de perfil no puede estar vacio"})
   id_profile: number;
 
 }
